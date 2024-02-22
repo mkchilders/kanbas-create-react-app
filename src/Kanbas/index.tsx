ca8a5c0f02e46458148b83c5f8ff08449a6452d0
mkchilders <childers.m@northeastern.edu>
2024-02-21 20:12:07 -0500

    Fix Kanbas nav width

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -6,10 +6,10 @@ function Kanbas() {
   return (
     <div className="d-flex">
       <KanbasNavigation />
-      <div style={{ flexGrow: 1 }}>
+      <div>
         <Routes>
             <Route path="/" element={<Navigate to="Dashboard" />} />
-            <Route path="Account" element={<h1>Account</h1>} />
+            <Route path="Account" element={<h1> Account</h1>} />
             <Route path="Dashboard" element={<Dashboard />} />
             <Route path="Courses/:courseId/*" element={<Courses />} />
         </Routes>
